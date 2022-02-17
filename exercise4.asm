041ee580622365ba5428f016126a0a3c3d775ca3
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-02-18 00:16:11 +0100

    Update and rename asm 17-07-2020 T2.asm to exercise4.asm

--- exercise4.asm
+++ exercise4.asm
@@ -1,23 +1,3 @@
-;# int elabora(char *st){
-;#      int i,conta;
-;#      conta=0;
-;#      for(i=0;i<8;i++)
-;#          conta=conta+st[i]-97 ;
-;#      return conta;
-;# }
-;# main() {
-;#      char ST[16];
-;#      int i, num;
-;#      for(i=0;i<3;i++){
-;#          printf("Inserisci una str con 8 lettere minuscole\n");
-;#          scanf("%s",ST);
-;#          if(strlen(ST)==8){
-;#              num=elabora(ST);
-;#              printf("Numero= %d\ n",num);
-;#          }
-;#      }
-;# }
-
 .data
 ST: .space 16
 stack: .space 32
